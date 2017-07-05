35533bf9cfbed2fae8232e82654f5b3bac564c05
SuperSaiyanScorpion <sheardb14@greyhigh.school.nz>
2017-07-05 12:58:51 +1200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,30 @@
+TITLE = "Ninja Square"
+WIDTH = 600
+HEIGHT = 500
+FPS =  60
+FONT_NAME = 'arial'
+HS_FILE = "highscore.txt"
+
+# Player properties
+PLAYER_ACC = 0.8
+PLAYER_FRICTION = -0.12
+PLAYER_GRAV = 0.8
+PLAYER_JUMP = 100
+# Starting platforms
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
+                 (125, HEIGHT - 350, 100, 20),
+                 (350, 200, 100, 20),
+                 (175, 100, 50, 20)]
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0) 
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+LIGHTBLUE = (0, 155, 155)
+BGCOLOR = LIGHTBLUE
+CRIMSON = (220, 20, 60)
+GOLD = (255, 215, 0)
